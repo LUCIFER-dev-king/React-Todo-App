78bca91a8d427ace1c3ccf3f43c98d4f38c7aedc
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-04-20 14:23:34 +0530

    initial commit

---
+++ src/context/reducer.js
@@ -0,0 +1,13 @@
+import { ADD_TODO, REMOVE_TODO } from "./action.types";
+
+export default (state, actions) => {
+  switch (actions.type) {
+    case ADD_TODO:
+      return [...state, actions.payload];
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== actions.payload);
+
+    default:
+      return state;
+  }
+};
